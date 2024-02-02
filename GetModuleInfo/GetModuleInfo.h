7e820d04db78407214ac1a0a767ab0aa34f5b65c
congcong.zhang <zccbjut@163.com>
2024-02-02 11:10:40 +0800

    modify function comments

--- GetModuleInfo/GetModuleInfo.h
+++ GetModuleInfo/GetModuleInfo.h
@@ -12,28 +12,28 @@ namespace ModuleInfo {
 							//示例：ProductName ==> GetModul.exe((2024-02-01 11:05:32))
 	};
 
-	/**
+	/*
 	 * @brief 获取模块以下属性信息
-	 *  Comments			附加信息或注释。
-        CompanyName			开发公司的名称。
-        FileDescription		描述。
-        FileVersion			版本号。
-        InternalName		内部名称，通常是开发时使用的名称。
-        LegalCopyright		版权信息。
-        LegalTrademarks		商标信息。
-        OriginalFilename	原始名称。
-        ProductName			产品名称。
-        ProductVersion		产品版本。
-        PrivateBuild		标识这个文件是一个私有版本。
-        SpecialBuild		对于特殊构建的说明。
-	 *
+	 * 	Comments			附加信息或注释。
+	 *  CompanyName			开发公司的名称。
+	 *  FileDescription		描述。
+	 *  FileVersion			版本号。
+	 *  InternalName		内部名称，通常是开发时使用的名称。
+	 *  LegalCopyright		版权信息。
+	 *  LegalTrademarks		商标信息。
+	 *  OriginalFilename	原始名称。
+	 *  ProductName			产品名称。
+	 *  ProductVersion		产品版本。
+	 *  PrivateBuild		标识这个文件是一个私有版本。
+	 *  SpecialBuild		对于特殊构建的说明。
+	 * 
 	 * @param fileName 模块名称
 	 * @param attrName 属性名称
 	 * @return 属性值
 	 */
 	std::string GetFileSingleInfo(const std::string& fileName, const std::string& attrName);
 
-	/**
+	/*
 	 * @brief 获取文件修改时间
 	 *
 	 * @param fileName 文件名称
@@ -41,16 +41,16 @@ namespace ModuleInfo {
 	 */
 	std::string GetModuleLastWriteTime(const std::string& fileName);
 
-	/**
+	/*
 	 * @brief 获取模块信息
 	 *
 	 * @param moduleName 模块名称
 	 * @param infoType 信息类型，参考ModuleInfoType，默认只输出模块名称
-	 * @return
+	 * @return 模块信息
 	 */
 	std::string GetModuleInfo(const std::string& moduleName, size_t infoType = MIT_EMPTY);
 	
-	/**
+	/*
 	 * @brief 获取模块名称
 	 *
 	 * @param 模块不完整的名称
@@ -58,14 +58,14 @@ namespace ModuleInfo {
 	 */
 	std::string GetModuleName(const std::string& target);
 	
-	/**
+	/*
 	 * @brief 获取当前进程调用的所有模块名称
 	 *
 	 * @return 所有模块名称
 	 */
 	std::unordered_set<std::string> GetModuleNames();
 	
-	/**
+	/*
 	 * @brief 获取当前进程所有模块信息
 	 *
 	 * @param infoType 信息类型，参考ModuleInfoType，默认只输出模块名称
@@ -73,7 +73,7 @@ namespace ModuleInfo {
 	 */
 	std::unordered_set<std::string> GetModuleInfo(size_t infoType = MIT_EMPTY);
 	
-	/**
+	/*
 	 * @brief 判断信息类型是否符合要求
 	 *
 	 * @param infoType 传入的信息类型
